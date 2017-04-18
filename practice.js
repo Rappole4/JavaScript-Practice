a08ca563ba9bb384fe1c19f303e519acb05fb45a
Rob n Heidi <rappole4@yahoo.com>
2017-04-18 09:18:54 -0400

    three answers

--- practice.js
+++ practice.js
@@ -8,3 +8,21 @@ for (i=0; i<array.length; i++){
 
 invert([0 , -32 , 66]);
 // end
+
+// string cleaning
+function stringClean(s){
+   return s.replace(/[0-9]/g, '');
+}
+// end
+
+// difference of volumes of cuboids
+function find_difference(a, b) {
+  var A = (a[0]*a[1]*a[2])
+  var B = (b[0]*b[1]*b[2])
+  if (A > B){
+  return (A - B);
+  }
+  else {
+  return (B - A);
+  }
+}// end
